38f8729a84a993ffa0d4a01a97df243ef358c492
whytetris <ys171714@gmail.com>
2023-04-28 15:59:58 +0500

    1

--- scp.js
+++ scp.js
@@ -2,6 +2,15 @@ let tg = window.Telegram.WebApp; //получаем объект webapp теле
 
 tg.expand(); //расширяем на все окно  
 
+let usercard = document.getElementById("usercard"); //получаем блок usercard 
+
+let profName = document.getElementById('p'); //создаем параграф
+profName.innerText = `${tg.initDataUnsafe.user.first_name}
+${tg.initDataUnsafe.user.last_name}
+${tg.initDataUnsafe.user.username} (${tg.initDataUnsafe.user.language_code})`;
+//выдем имя, "фамилию", через тире username и код языка
+usercard.appendChild(profName); //добавляем 
+
 let userid = document.createElement('p'); //создаем еще параграф 
 userid.innerText = `${tg.initDataUnsafe.user.id}`; //показываем user_id
 usercard.appendChild(userid); //добавляем
@@ -43,12 +52,5 @@ Telegram.WebApp.onEvent('mainButtonClicked', function(){
 });
 
 
-let usercard = document.getElementById("usercard"); //получаем блок usercard 
 
-let profName = document.createElement('p'); //создаем параграф
-profName.innerText = `${tg.initDataUnsafe.user.first_name}
-${tg.initDataUnsafe.user.last_name}
-${tg.initDataUnsafe.user.username} (${tg.initDataUnsafe.user.language_code})`;
-//выдем имя, "фамилию", через тире username и код языка
-usercard.appendChild(profName); //добавляем 
 
